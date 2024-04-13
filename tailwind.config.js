7899d2ba9fb49688d04f9f4d38f61820ab877649
Piperja <70609909+Piperja@users.noreply.github.com>
2024-04-13 20:05:47 +0000

    ugh

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html",
+"./src/index.js",
+"./src/images/heart-solid.svg"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
